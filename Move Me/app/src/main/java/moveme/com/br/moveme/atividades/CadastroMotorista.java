0524505b1cfb34e2b7c55dddfbc01e8dc983dc37
Karol <karolaynesantos63@outlook.com>
2019-06-09 23:26:39 -0300

    Atualizações Login Motorista

--- Move Me/app/src/main/java/moveme/com/br/moveme/atividades/CadastroMotorista.java
+++ Move Me/app/src/main/java/moveme/com/br/moveme/atividades/CadastroMotorista.java
@@ -15,6 +15,30 @@ public class CadastroMotorista extends AppCompatActivity {
         setContentView(R.layout.activity_cadastro_motorista);
         assert getSupportActionBar() != null;   //null check
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        /*
+          mRegistration = (Button) findViewById(R.id.registration);
+
+        mRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = mEmail.getText().toString();
+                final String password = mPassword.getText().toString();
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(DriverLoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            Toast.makeText(DriverLoginActivity.this, "sign up error", Toast.LENGTH_SHORT).show();
+                        }else{
+                            String user_id = mAuth.getCurrentUser().getUid();
+                            DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child("Drivers").child(user_id).child("name");
+                            current_user_db.setValue(email);
+                        }
+                    }
+                });
+            }
+        });
+        */
     }
     @Override
     public boolean onSupportNavigateUp(){
